e8b91b9539b1d103100eb9853898724805e35c5b
tummanoonwancha-em <trialation.development@gmail.com>
2019-12-09 08:55:54 +0700

    Revise debugging php on server

--- @proto/app.php
+++ @proto/app.php
@@ -96,3 +96,12 @@
         }
 
     }
+
+    if(App::$debuggingMode){
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    }else{
+        ini_set('display_errors', 0);
+        ini_set('display_startup_errors', 0);
+    }
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+    $dir = "./"; //current directory
+    include_once $dir . '@proto/app.php'; 
+    App::include_protop($dir);
